f1c4576f9909f11bbeebc661d44eaab5ed88102e
Anna Kolmogorova <iveina2021@gmail.com>
2022-08-16 21:03:58 +0200

    add parametrized test

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,31 +1,57 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
+import javax.naming.Name;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
+    public IngredientType ingredientType;
+    public String name;
+    public float price;
+
+    public IngredientTest(IngredientType ingredientType, String name, float price) {
+        this.ingredientType = ingredientType;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] parameters() {
+        return new Object[][]{
+                {IngredientType.SAUCE, "Соус традиционный галактический", 15},
+                {IngredientType.FILLING, "Сыр с астероидной плесенью", 4142},
+        };
+    }
 
     @Test
     public void ingredientGetType() {
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Соус традиционный галактический", 15);
+        Ingredient ingredient = new Ingredient(ingredientType, name, price);
+
         IngredientType actual = ingredient.getType();
-        assertThat(actual).isEqualTo(IngredientType.SAUCE);
+
+        assertThat(actual).isEqualTo(ingredientType);
     }
 
     @Test
     public void ingredientGetPrice() {
-        float price = 4142;
-        Ingredient ingredient = new Ingredient(IngredientType.FILLING, "Сыр с астероидной плесенью", price);
+        Ingredient ingredient = new Ingredient(ingredientType, name, price);
+
         float actual = ingredient.getPrice();
+
         assertThat(actual).isEqualTo(price);
     }
 
     @Test
     public void ingredientGetName() {
-        String name = "Соус традиционный галактический";
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, name, 15);
+        Ingredient ingredient = new Ingredient(ingredientType, name, price);
+
         String actual = ingredient.getName();
+
         assertThat(actual).isEqualTo(name);
     }
 }
